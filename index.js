715806e0076f144a22dd2fad342f6539e73605ea
Amritpalwaraich <amritpalsinghwarich@gmail.com>
2025-08-11 21:08:42 +0530

    express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello Amrit pal")
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
+app.get('/twitter', (req, res) => {
+    res.send("your on twitter for working ")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listen on port ${port}`)
+})
