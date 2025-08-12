416322ea9ad10ca7a9a59de60985d2609a050fd0
Amritpalwaraich <amritpalsinghwarich@gmail.com>
2025-08-12 23:18:55 +0530

    express

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
+app.get("/login", (req, res) => {
+    res.send(" coding using chai channel ")
+})
+
 app.get('/twitter', (req, res) => {
     res.send("your on twitter for working ")
 })
