20775389433dc0c7402a7340013315509644d22a
Amritpalwaraich <amritpalsinghwarich@gmail.com>
2025-08-17 22:02:11 +0530

    h

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req, res) => {
-    res.send
-
-})
+app.get('/', (req, res))
 
 app.listen(port, () => {
     console.log(`Example app listen on port ${port}`)
