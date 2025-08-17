df3435a230abdd7926f1f0f63b90a367eef536f4
Amritpalwaraich <amritpalsinghwarich@gmail.com>
2025-08-17 22:01:40 +0530

    high

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 const port = 3000
 
 
+app.get('/', (req, res) => {
+    res.send('working high on  ')
 
+})
 
 app.listen(port, () => {
     console.log(`Example app listen on port ${port}`)
